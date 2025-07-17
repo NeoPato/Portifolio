9bc96b531ececf74b93b30ccddf91ab317df38df
NeoPato <gabrielhmsena20@gmail.com>
2025-07-17 02:09:40 -0300

    quase3

--- portifolio/src/components/Card.jsx
+++ portifolio/src/components/Card.jsx
@@ -1,16 +1,11 @@
 const Card = (props) => {
   return(
       <div class="card">
-          <img src={props.imagem}></img>
-            <p>{props.Nome}</p>
-            <p>{props.Idade}</p>
-            <p>{props.DatadeNascimento}</p>       
             <p>{props.CursoTec}</p>
             <p>{props.CursoGraduação}</p>
             <p>{props.CursoExtra}</p>
             <p>{props.CursoExtra2}</p>
             <p>{props.CursoExtra3}</p>
-            <p>{props.Localização}</p>
             <p>{props.Linguagens}</p>
             <p>{props.Telefone}</p>
             <p>{props.Gmail}</p>
