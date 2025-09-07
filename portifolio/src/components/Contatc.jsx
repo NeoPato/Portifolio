46acd8a1b6656e77f6ac16141cd4bbf9da66d274
Gabriel Sena <85597607+NeoPato@users.noreply.github.com>
2025-09-07 20:30:13 -0300

    Atualizar o Contatc.jsx

--- portifolio/src/components/Contatc.jsx
+++ portifolio/src/components/Contatc.jsx
@@ -14,7 +14,17 @@ export function Contatc() {
             <div className="card-identity">
               <h2>Sobre Mim</h2>
               <p>
-                Meu nome é Gabriel Henrique de Matos Sena, sou estudante de Engenharia de Software no UniSenai, e <br />técnico em Desenvolvimento de Sistemas. Adquiri muitos conhecimentos na área do front-end, mas <br />também tendo um conhecimento em back-end.
+                Gabriel Henrique de Matos Sena – Estudante de Engenharia de Software
+Sou um estudante dedicado de Engenharia de Software, com sólida base em desenvolvimento de sistemas e aplicações web.
+
+Perfil
+Motivado pela busca constante de aprendizado e prática em projetos, possuo perfil proativo, comunicativo e adaptável, sempre em evolução para aprimorar minhas habilidades técnicas e interpessoais.
+
+Portfólio
+Atualmente, desenvolvo e compartilho minhas habilidades no meu portfólio online.
+
+Objetivo
+Busco oportunidades para crescer profissionalmente na área de tecnologia, contribuindo com soluções criativas e eficientes.
               </p>
             </div>
             <div className='card-education'>
